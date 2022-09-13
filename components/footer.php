7343164b769dd31ba6a3e83d4fc8ee6c7c0aa64c
Ejiroghene <94175016+OjerIsaac@users.noreply.github.com>
2022-09-13 18:41:23 +0100

    update: minor

--- components/footer.php
+++ components/footer.php
@@ -1,5 +1,6 @@
 <!---  JavaScripts  --->
-<script src="../assets/js/jquery.min.js"></script>        <!---  jQuery  --->
+<script src="../assets/js/jquery.min.js"></script>       <!---  jQuery  --->
+<script src="../assets/js/jquery.dataTables.min.js"></script> 
 <script src="../assets/js/bootstrap.min.js"></script>     <!---  Bootstrap  --->
 <script src="../assets/js/all.js"></script>               <!---  Font-awesome  --->
 <script src="../assets/js/register.js"></script>          <!---  Main script  --->
